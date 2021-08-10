5dfce91e30a1ac7076d1e69e58815d16784dff7f
SCESecure <secure@msub.kr>
2021-08-10 12:15:02 +0900

    버그 수정 & 기능과 내용 보충

--- N Converter/MainSource.c
+++ N Converter/MainSource.c
@@ -17,7 +17,7 @@ MAIN :
 	// 메인 화면
 	system("cls");
 	printf("====================\n");
-	printf("<수 변환기>\n\n");
+	printf("<수 변환기 1.0>\n\n");
 	printf("1. 삼각비 값\n");
 	printf("2. e^x 값\n");
 	printf("3. log 값\n");
@@ -45,10 +45,10 @@ Minput :
 
 			printf("삼각비 값으로 바꿀 값을 입력해주세요.\n\n");
 			printf("입력 : ");
-			scanf("%lf", &t);
+			scanf_s("%lf", &t);
 			fseek(stdin, 0, SEEK_SET);
 
-			printf("\n입력된 수 : %d\n\n", t);
+			printf("\n입력된 수 : %lf\n\n", t);
 			printf("사인 : %lf\n", sin(t));
 			printf("코사인 : %lf\n", cos(t));
 			printf("탄젠트 : %lf\n\n", tan(t));
@@ -58,7 +58,7 @@ Minput :
 			printf("아크 코사인 : %lf\n\n", acos(t));
 			printf("하이퍼볼릭 사인 : %lf\n", sinh(t));
 			printf("하이퍼볼릭 코사인 : %lf\n", cosh(t));
-			printf("하이퍼볼릭 탄젠트 : %lf\n", tanh(t));
+			printf("하이퍼볼릭 탄젠트 : %lf\n\n", tanh(t));
 
 		input1:
 
@@ -78,13 +78,13 @@ Minput :
 
 			double ex = 0;
 
-			printf("e^x(e의 x승, 단 e는 자연상수)에서 x의 값을 입력해주세요.\n");
+			printf("e^x(e의 x승, 단 e는 자연상수)에서 x의 값을 입력해주세요.\n\n");
 			printf("입력 : ");
-			scanf("%lf", &ex);
+			scanf_s("%lf", &ex);
 			fseek(stdin, 0, SEEK_SET);
 
 			printf("\n입력된 수 : %lf\n\n", ex);
-			printf("e^x : %lf\n", exp(ex));
+			printf("e^x : %lf\n\n", exp(ex));
 
 		input2:
 
@@ -104,14 +104,14 @@ Minput :
 
 			double nlog = 0;
 
-			printf("log 값으로 바꿀 수를 입력해주세요.\n");
+			printf("log 값으로 바꿀 수를 입력해주세요.\n\n");
 			printf("입력 : ");
-			scanf("%lf", &nlog);
+			scanf_s("%lf", &nlog);
 			fseek(stdin, 0, SEEK_SET);
 
 			printf("\n입력된 수 : %lf\n\n", nlog);
 			printf("log값 : %lf\n", log(nlog));
-			printf("log10 값 : %lf\n", log10(nlog));
+			printf("log10 값 : %lf\n\n", log10(nlog));
 
 		input3:
 
@@ -131,13 +131,13 @@ Minput :
 
 			double nabs = 0;
 
-			printf("절댓값을 구할 수를 입력해주세요\n");
+			printf("절댓값을 구할 수를 입력해주세요\n\n");
 			printf("입력 : ");
-			scanf("%lf", &nabs);
+			scanf_s("%lf", &nabs);
 			fseek(stdin, 0, SEEK_SET);
 
 			printf("\n입력된 수 : %lf\n\n", nabs);
-			printf("절댓값 : %lf\n", fabs(nabs));
+			printf("절댓값 : %lf\n\n", fabs(nabs));
 
 		input4:
 
@@ -156,9 +156,9 @@ Minput :
 
 			double nsqrt = 0;
 
-			printf("'루트 n'에서 n의 값을 입력해주세요\n");
+			printf("'루트 n'에서 n의 값을 입력해주세요\n\n");
 			printf("입력 : ");
-			scanf("%lf", &nsqrt);
+			scanf_s("%lf", &nsqrt);
 			fseek(stdin, 0, SEEK_SET);
 
 			printf("\n입력된 수 : %lf\n\n", nsqrt);
@@ -182,15 +182,16 @@ Minput :
 			double xpow = 0;
 			double ypow = 0;
 
-			printf("x^y(x의 y승)의 값들을 입력해주세요\n");
-			printf("x 입력 : ");
-			scanf("%lf", &xpow);
+			printf("x^y(x의 y승)의 값들을 입력해주세요\n\n");
+			printf("x 값 입력 : ");
+			scanf_s("%lf", &xpow);
 			fseek(stdin, 0, SEEK_SET);
-			scanf("%lf", ypow);
+			printf("y 값 입력 : ");
+			scanf_s("%lf", &ypow);
 			fseek(stdin, 0, SEEK_SET);
 
 			printf("\n입력된 수 : x = %lf, y = %lf\n\n", xpow, ypow);
-			printf("x^y 값 : %lf\n", pow(xpow, ypow));
+			printf("x^y 값 : %lf\n\n", pow(xpow, ypow));
 
 		input6:
 
@@ -204,21 +205,56 @@ Minput :
 				goto input6;
 			}
 		case 55: // fmod
+		FMOD :
+
 			system("cls");
 
 			double xmod = 0;
 			double ymod = 0;
 
-			printf("x를 y로 나눈 나머지 값을 구할 때 값을 입력해주세요\n");
-			printf("x 입력 : ");
-			scanf("%lf", &xmod);
+			printf("x를 y로 나눈 나머지 값을 구할 때 값을 입력해주세요\n\n");
+			printf("x 값 입력 : ");
+			scanf_s("%lf", &xmod);
 			fseek(stdin, 0, SEEK_SET);
-			printf("y 입력 : ");
-			scanf("%lf", &ymod);
+			printf("y 값 입력 : ");
+			scanf_s("%lf", &ymod);
 			fseek(stdin, 0, SEEK_SET);
 
+			if (xmod < ymod) {
+				system("cls");
+				printf("입력하실 때 x 값이 y 값보다 커야합니다. (백 스페이스 눌러 돌아가기)\a\n");
+				
+			ERinput1 :
+
+				key = _getch();
+				subkey = inputkeys(key);
+				if (subkey == 1) {
+					goto FMOD;
+				}
+				else {
+					printf("백 스페이스키를 입력해주시길 바랍니다.\a\n");
+					goto ERinput1;
+				}
+			}
+			else if (ymod == 0) {
+				system("cls");
+				printf("y 값이 0이 될 수 없습니다. (백 스페이스 눌러 돌아가기)\a\n");
+
+			ERinput2:
+
+				key = _getch();
+				subkey = inputkeys(key);
+				if (subkey == 1) {
+					goto FMOD;
+				}
+				else {
+					printf("백 스페이스키를 입력해주시길 바랍니다.\a\n");
+					goto ERinput2;
+				}
+			}
+
 			printf("\n입력된 수 : x = %lf, y = %lf\n\n", xmod, ymod);
-			printf("나머지 값 : %lf\n", fmod(xmod, ymod));
+			printf("나머지 값 : %lf\n\n", fmod(xmod, ymod));
 
 		input7:
 
@@ -259,7 +295,20 @@ Minput :
 		}
 	}
 	else {
-		printf("이미 메인화면에 있습니다.\a");
+	system("cls");
+		printf("이미 메인화면에 있습니다.\a\n");
+	MERRinput:
+
+		key = _getch();
+		subkey = inputkeys(key);
+
+		if (subkey == 1) {
+			goto MAIN;
+		}
+		else {
+			printf("백 스페이스 키를 입력해주시길 바랍니다.\n");
+			goto input7;
+		}
 	}
 	
 }
