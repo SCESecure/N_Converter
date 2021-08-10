ecdfadb286b23eb655afb1e36ae424f3f7086009
SCESecure <secure@msub.kr>
2021-08-10 11:19:21 +0900

    삼각비 값 추가

--- N Converter/MainSource.c
+++ N Converter/MainSource.c
@@ -1,5 +1,98 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
+#include <conio.h>
+#include <Windows.h>
+#include <math.h>
+
+int inputkeys(int keynum);
 
 int main() {
-	printf("Hello World\n");
+	// 전역 변수
+	int key = 0;
+	int subkey = 0;
+	
+MAIN :
+
+	// 메인 화면
+	system("cls");
+	printf("====================\n");
+	printf("<수 변환기>\n\n");
+	printf("1. 삼각비 값\n");
+	printf("2. e^x 값\n");
+	printf("3. log 값\n");
+	printf("4. 절댓값\n");
+	printf("5. 제곱근 값\n");
+	printf("6. x^y 값\n");
+	printf("7. 나머지 값\n");
+	printf("====================\n");
+
+	key = _getch();
+	subkey = inputkeys(key);
+
+	// 서브 화면
+
+	if (subkey == 2) {
+		switch (key)
+		{
+		case 49 :
+			system("cls");
+
+			double t = 0;
+
+			printf("삼각비 값으로 바꿀 값을 입력해주세요.\n");
+			printf("입력 : ");
+			scanf("%lf", &t);
+
+			printf("\n 사인 : %lf\n", sin(t));
+			printf("코사인 : %lf\n", cos(t));
+			printf("탄젠트 : %lf\n\n", tan(t));
+			printf("아크 사인 : %lf\n", asin(t));
+			printf("아크 코사인 : %lf\n", acos(t));
+			printf("아크 탄젠트 : %lf\n", atan(t));
+			printf("아크 코사인 : %lf\n\n", acos(t));
+			printf("하이퍼볼릭 사인 : %lf\n", sinh(t));
+			printf("하이퍼볼릭 코사인 : %lf\n", cosh(t));
+			printf("하이퍼볼릭 탄젠트 : %lf\n", tanh(t));
+
+		input1 :
+
+			key = _getch();
+			subkey = inputkeys(key);
+			if (subkey == 1) {
+				goto MAIN;
+			}
+			else {
+				printf("백 스페이스 키를 입력해주시길 바랍니다.\n");
+				goto input1;
+			}
+
+		case 50 :
+		case 51 :
+		case 52 :
+		case 53 :
+		case 54 :
+		case 55 :
+		default:
+			break;
+		}
+	}
+	else {
+		printf("이미 메인화면에 있습니다.\a");
+	}
+	
+}
+
+int inputkeys(int keynum) {
+
+	int keyr = 0;
+
+	if (keynum == 8) {
+		keyr = 1;
+		return keyr;
+	}
+	else {
+		keyr = 2;
+		return keyr;
+	}
 }
